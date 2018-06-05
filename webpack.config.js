eeccf730e49e110ad7774906dfc6aa371304742f
liangwei02 <liangwei02@wangcaigu.com>
2018-06-05 15:32:14 +0800

    提交完成配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
 const path = require('path');
 const glob = require('glob');
-const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 const extractLess = new ExtractTextPlugin({
-    filename: "assets/css/[name][hash:8].css",
-    disable: process.env.NODE_ENV === "development"
+    filename: "assets/css/[name][hash:8].css"
 });
 /*
 * 获取文件路径
@@ -16,54 +14,66 @@ const extractLess = new ExtractTextPlugin({
 var JsFiles = glob.sync('./src/**/**.js'),
     newEntries = {},
     config = {
-    entry: newEntries,
-    output: {
-        filename: 'assets/js/[name][chunkhash:8].js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: ""///public/static/
-    },
-    module: {
-        rules: [
-            {
-                test: /\.less$/,
-                use: extractLess.extract({
+        entry: newEntries,
+        output: {
+            filename: 'assets/js/[name][chunkhash:8].js',
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: ""///public/static/
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
                     use: [
-                        'css-loader',
-                        'less-loader'
-                    ]
-                })
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'wdjf-file-loader',
-                        options: {
-                            name: '[path][name].[ext]',
-                            outputPath:'assets/images/'
+                        'vue-style-loader',
+                        'css-loader'
+                    ],
+                },
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.less$/,
+                    use: extractLess.extract({
+                        use: [
+                            'css-loader',
+                            'less-loader'
+                        ]
+                    })
+                },
+                {
+                    test: /\.(png|svg|jpg|gif)$/,
+                    use: [
+                        {
+                            loader: 'wdjf-file-loader',
+                            options: {
+                                name: '[path][name].[ext]',
+                                outputPath: 'assets/images/'
+                            }
                         }
-                    }
-                ]//   d:work\learn-webpacl\src\index\index.png  /src/promotion/active20180531/index.png
-            },
-            {
-                test: /\.html$/,
-                use: ['html-withimg-loader']
-            }
+                    ]//   d:work\learn-webpacl\src\index\index.png  /src/promotion/active20180531/index.png
+                },
+                {
+                    test: /\.html$/,
+                    use: ['html-withimg-loader']
+                }
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(['dist']),
+            extractLess
         ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        extractLess
-    ]
-};
-JsFiles.forEach(function(f){
-    var name = /.*?\/(.*?)\.js/.exec(f)[1].replace('src/','').replace("/js","");//得到apps/question/index这样的文件名
+    };
+JsFiles.forEach(function (f) {
+    var name = /.*?\/(.*?)\.js/.exec(f)[1].replace('src/', '').replace("/js", "");//得到apps/question/index这样的文件名
     newEntries[name] = f;
 
-    var plug =  new HtmlWebpackPlugin({
-        filename: path.resolve(__dirname, './dist/pages/'+[name]+'[hash:8].html'),
-        chunks:[name],//需要引入的chunk，不配置就会引入所有页面的资源
-        template: path.resolve(__dirname, './src/'+ name +'.html'),
+    var plug = new HtmlWebpackPlugin({
+        filename: path.resolve(__dirname, './dist/pages/' + [name] + '.html'),
+        chunks: [name],//需要引入的chunk，不配置就会引入所有页面的资源
+        template: path.resolve(__dirname, './src/' + name + '.html'),
         inject: true       //不会自动写入编译的css和js
     });
 
