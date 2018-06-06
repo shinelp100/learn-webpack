8c0fe33cdbd532c391610c00ea657f4e9e556b2f
liangwei02 <liangwei02@wangcaigu.com>
2018-06-06 18:22:25 +0800

    package文件修改

--- src/list/index.js
+++ src/list/index.js
@@ -2,7 +2,7 @@
 * 添加样式文件的引用
 * */
 import './index.less';
-import './index.html';
+
 
 const util = require("../../utils/util");
 
@@ -11,3 +11,24 @@ function getUserName() {
 }
 getUserName();
 util.getUserAge();
+
+var axios = require('axios');
+var MockAdapter = require('axios-mock-adapter');
+
+// This sets the mock adapter on the default instance
+var mock = new MockAdapter(axios);
+
+// Mock GET request to /users when param `searchText` is 'John'
+// arguments for reply are (status, data, headers)
+mock.onGet('/users', { params: { searchText: 'John' } }).reply(200, {
+    users: [
+        { id: 1, name: 'John Smith' }
+    ]
+});
+
+axios.get('/users', { params: { searchText: 'John11' } } )
+    .then(function(response) {
+        console.log(response.data);
+    }).catch(function(error){
+        console.log(error);
+    });
