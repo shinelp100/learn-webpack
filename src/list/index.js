f86f79be8d72118e4c8bffd3915b4a2e65472ef9
liangwei02 <liangwei02@wangcaigu.com>
2018-06-07 23:33:06 +0800

    npm -i loadsh

--- src/list/index.js
+++ src/list/index.js
@@ -20,15 +20,20 @@ var mock = new MockAdapter(axios);
 
 // Mock GET request to /users when param `searchText` is 'John'
 // arguments for reply are (status, data, headers)
-mock.onGet('/users', { params: { searchText: 'John' } }).reply(200, {
-    users: [
-        { id: 1, name: 'John Smith' }
-    ]
+mock.onGet('/users', { params: { searchText: 'John' } }).reply(2000, {
+    message:"未知错误1122   "
 });
 
-axios.get('/users', { params: { searchText: 'John11' } } )
+axios.get('/users', { params: { searchText: 'John' } } )
     .then(function(response) {
         console.log(response.data);
     }).catch(function(error){
-        console.log(error);
+        console.log(error.response.data.message);
     });
+
+
+
+// Load the full build.
+var _ = require('lodash');
+console.log(_.trim('  a a b b c c c  '));
+console.log(_.trim('-_-abc-_-','_-'));
